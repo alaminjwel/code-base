fc806c4432ff5b7aa0c0375c10ab685eefe48a1e
alaminjwel <jwel.cse@gmail.com>
2023-11-04 22:12:12 +0600

    sync

---
+++ leetcode/python/338. Counting Bits.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def countBits(self, num: int) -> List[int]:
+        dp = [0]
+        for i in range(1, num + 1):
+            if i % 2 == 1:
+                dp.append(dp[i - 1] + 1)
+            else:
+                dp.append(dp[i // 2])
+        return dp
+
+obj = Solution()
+print(obj.countBits(5))
